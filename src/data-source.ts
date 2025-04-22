cc39317f343c17444649706d196692663274679a
chaitanya2744 <chaitanya@clicentrix.com>
2025-04-22 11:29:43 +0530

    lead assignment

--- src/data-source.ts
+++ src/data-source.ts
@@ -25,7 +25,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DATABASE_USER_NAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: ["error"],
   // logging:true,
   entities: entitiesPath,
